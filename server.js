0d0618dbdbbe01f991b074fc0f3c8d2402fc6728
Alyson May <amay1421@yahoo.com>
2021-03-01 17:31:47 -0600

    deploying

--- server.js
+++ server.js
@@ -15,10 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/myFirstDatabase", {
   useNewUrlParser: true,
-  useFindAndModify: false
-});
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
