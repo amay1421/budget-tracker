6da8f5533c0dd2318313a643dd545f1a44415e23
Alyson May <amay1421@yahoo.com>
2021-03-01 17:36:05 -0600

    pushing

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/myFirstDatabase", {
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
